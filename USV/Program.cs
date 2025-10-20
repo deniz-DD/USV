65ca299958567932201604f7a957b4a132634f0e
Deniz <deniz_dural@yahoo.com>
2025-10-20 16:00:18 +0200

    input

--- USV/Program.cs
+++ USV/Program.cs
@@ -12,8 +12,10 @@ namespace USV
 
         {
             int number_devices = 1;
-            int number_loop = 1;
-            Console.Write("Bitte geben sie an wie viele Geraete Sie an der USV anschliessen wollen: ");
+            int number_loop = 0;
+            double power;
+           
+            Console.Write("Bitte geben sie an wie viele Akkus wollen Sie an der USV anschliessen wollen: ");
             try
             {
                  number_devices = Convert.ToInt32(Console.ReadLine());
@@ -21,16 +23,23 @@ namespace USV
             {
                 Console.WriteLine("Du hast keine Zahl angegben: " + m.Message);
             }
-
+            double[] powerList = new double[number_devices];
 
 
             do
             {
-                Console.WriteLine(number_loop);
+                Console.Write("Bitte geben Sie die Spannung (V = Volt) an:" );
+                power = Convert.ToDouble(Console.ReadLine());   
+                powerList[number_loop] = power; 
 
                 number_loop++;  
 
-            } while (number_loop <= number_devices);
+            } while (number_loop < number_devices);
+            Console.WriteLine();
+            for (int i = 0; i < number_devices; i++)
+            {
+                Console.WriteLine(powerList[i]);
+            }
             Console.ReadKey();
         }
     }
