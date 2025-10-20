a0ad70de2c86fbfa5505e5756e23d1671f499048
Deniz <deniz_dural@yahoo.com>
2025-10-20 15:40:27 +0200

    InputUser_

--- USV/Program.cs
+++ USV/Program.cs
@@ -9,9 +9,29 @@ namespace USV
     internal class Program
     {
         static void Main(string[] args)
-        {
 
-            Console.Write("Testbranch");
+        {
+            int number_devices = 1;
+            int number_loop = 1;
+            Console.Write("Bitte geben sie an wie viele Geraete Sie an der USV anschliessen wollen: ");
+            try
+            {
+                 number_devices = Convert.ToInt32(Console.ReadLine());
+            }catch(FormatException m)
+            {
+                Console.WriteLine("Du hast keine Zahl angegben: " + m.Message);
+            }
+
+
+
+            do
+            {
+                Console.WriteLine(number_loop);
+
+                number_loop++;  
+
+            } while (number_loop <= number_devices);
+            Console.ReadKey();
         }
     }
 }
