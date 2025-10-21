fe94ce286109fb8580498f205c61bf304c5ab59e
Deniz <deniz_dural@yahoo.com>
2025-10-21 13:15:40 +0200

    eqndjsan

--- USV/Program.cs
+++ USV/Program.cs
@@ -11,92 +11,109 @@ namespace USV
         static void Main(string[] args)
 
         {
+
             int number_devices = 1;
-            int number_loop = 0;
+            int number_loop = 1;
             string S_power;
             string S_cap;
             double power;
             double cap;
-            double all_power = 0;
-            double all_cap = 0; 
-            double Wirkleistung = 0;    
-            double Gesamtleistung = 0;  
-           
+            double all_power = 0; //Scheinleistung 
+            double all_cap = 0;
+            double Wirkleistung = 0;
+            double Gesamtleistung = 0;
+
             Console.Write("Bitte geben sie an wie viele Akkus wollen Sie an der USV anschliessen wollen: ");
             try
             {
-                 number_devices = Convert.ToInt32(Console.ReadLine());
-            }catch(FormatException m)
+                number_devices = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException m)
             {
                 Console.WriteLine("Du hast keine Zahl angegben: " + m.Message);
+
             }
             double[] powerList = new double[number_devices];
-            double[] capList= new double[number_devices];
-
-
+            double[] capList = new double[number_devices];
             do
             {
-                Console.Write("Bitte geben Sie die Spannung (V = Volt) an: " );
-                //power = Convert.ToDouble(Console.ReadLine());   
+
+                Console.WriteLine("-----" + "Akku " + number_loop + "-----");
+                Console.Write("Bitte geben Sie die Spannung (VA = Volt * Ampere) an: ");
                 S_power = Console.ReadLine();
-                if(S_power == ""){ //Standard Wert von 12A
+               
+                if (S_power == "")
+                { //Standard Wert von 12VA
                     power = 12;
                     powerList[number_loop] = power;
                     all_power += powerList[number_loop];
-                    number_loop++;
+                   
                 }
                 else if (!double.TryParse(S_power, out power))
                 {
-                    Console.WriteLine("Das ist keine Zahl");
-                   
+                    do
+                    {
+                         Console.Write("Bitte geben Sie die Spannung (VA = Volt * Ampere) an: ");
+                         S_power = Console.ReadLine();
+                        if (S_power == "")
+                        { //Standard Wert von 12VA
+                            power = 12;
+                            powerList[number_loop] = power;
+                            all_power += powerList[number_loop];
+
+                        }
+                        else
+                        {
+                            powerList[number_loop - 1] = power;
+                            all_power += powerList[number_loop - 1];
+                        }
+                    } while (!double.TryParse(S_power, out power));
+                    Console.WriteLine("Weiter");
                 }
                 else
                 {
-                    powerList[number_loop] = power;
-                    all_power += powerList[number_loop];
-                    number_loop++;
+                    powerList[number_loop-1] = power;
+                    all_power += powerList[number_loop-1];
                 }
-
-
-            } while (number_loop < number_devices);
-            Console.WriteLine(all_power + "V");
-            Wirkleistung = all_power * 0.65;
-            Gesamtleistung = Wirkleistung * 1.30;
-            Console.WriteLine("______________");
-            Console.WriteLine("Wirkleistung: " + Wirkleistung);
-            Console.WriteLine("______________");
-            Console.WriteLine("Gesamtlast: " + Gesamtleistung);
-            number_loop = 0;
-            do
-            {
-                Console.Write("Bitte geben Sie die Kapazitaet ihrer Akkus an (Ah): ");
-                //power = Convert.ToDouble(Console.ReadLine());   
+                //Kapazitaet abfragen
+                Console.Write("Bitte geben Sie die Kapazitaet (Ah) an: ");
                 S_cap = Console.ReadLine();
                 if (S_cap == "")
-                { //Standard Wert von 0A
+                { //Standard Wert von 1Ah
                     number_loop++;
                 }
                 else if (!double.TryParse(S_cap, out cap))
                 {
                     Console.WriteLine("Das ist keine Zahl");
-
                 }
                 else
                 {
-                    capList[number_loop] = cap;
-                    all_cap += capList[number_loop];
+                    capList[number_loop - 1] = cap;
+                    all_cap += capList[number_loop - 1];
                     number_loop++;
                 }
 
 
-            } while (number_loop < number_devices);
-            Console.WriteLine("______________");
-            Console.WriteLine("Gesamtkapazittaet: "+all_cap);
+            } while (number_loop <= number_devices);
 
-            double time = (number_devices * all_power * all_cap)/ Gesamtleistung;   
-            Console.WriteLine(time);
+
+
+         
+            
+
+            //Wirkleistung = all_power * 0.65;
+            //Gesamtleistung = Wirkleistung * 1.30;
+            //Console.WriteLine("______________");
+            //Console.WriteLine("Wirkleistung: " + Wirkleistung);
+            //Console.WriteLine("______________");
+            //Console.WriteLine("Gesamtlast: " + Gesamtleistung);
+
+
+
 
             Console.ReadKey();
         }
+
+        
     }
 }
